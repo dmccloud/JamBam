3100c7f06cdb58c6ef122083d9202a7fa4ba84f3
dhmcclou22 <dhmcclou@asu.edu>
2021-10-06 14:48:40 -0700

    ReadMe

---
+++ src/Components/App/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      {/* Add a SearchBar component   */}
+      {/* <!-- Add a SearchResults component --> */}
+      {/* <!-- Add a Playlist component --> */}
+    </div>
+  );
+}
+
+export default App;
